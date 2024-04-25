739a1bda4a32645ee6c712e2b50b427ead4d112c
DESKTOP-3GUQ93R\GA2B <h1202315005@hiratagakuen.onmicrosoft.com>
2024-04-25 10:52:45 +0900

    セレクト画面SEとBGM追加

--- Assets/田中/Script/SceneChange.cs
+++ Assets/田中/Script/SceneChange.cs
@@ -5,7 +5,16 @@ using UnityEngine.SceneManagement; // 忘れない！！
 
 public class SceneChange : MonoBehaviour
 {
+    [SerializeField, Header("決定音")]
+    private GameObject submitSE;
+
     public void stage01()
+    {
+        Instantiate(submitSE);
+
+        Invoke("SceneChange1", 0.5f);
+    }
+    public void SceneChange1()
     {
         //シーン名をここに入力
         SceneManager.LoadScene("Stage1");
@@ -13,10 +22,25 @@ public class SceneChange : MonoBehaviour
 
     public void stage02()
     {
+        Instantiate(submitSE);
+
+        Invoke("SceneChange2", 0.5f);
+
+    }
+    public void SceneChange2()
+    {
         //シーン名をここに入力
         SceneManager.LoadScene("Stage2");
     }
+
     public void stage03()
+    {
+        Instantiate(submitSE);
+
+        Invoke("SceneChange3", 0.5f);
+
+    }
+    public void SceneChange3()
     {
         //シーン名をここに入力
         SceneManager.LoadScene("Stage3");
