2b0dc2feff07a136028c181c58da5f151be7c91c
tsunekuni5102 <h1202315005@hiratagakuen.onmicrosoft.com>
2024-05-14 09:56:25 +0900

    正解画面と問題移行画面を作成

---
+++ Assets/常国/spript/Button/NextButton.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // 忘れない！！
+
+public class NextButton : MonoBehaviour
+{
+    [SerializeField, Header("決定音")]
+    private GameObject submitSE;
+
+    public void OnClick()
+    {
+        Instantiate(submitSE);
+
+        Invoke("SceneChange", 0.5f);
+    }
+
+    public void SceneChange()
+    {
+        //シーン名をここに入力
+        SceneManager.LoadScene("Stage3 qestion");
+    }
+
+}
+
+
