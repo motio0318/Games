860bdc1dce25e2d3e14c0a4f4963a32fe668ac21
DESKTOP-CMFB67U\GA2B <h1202316001@hiratagakuen.onmicrosoft.com>
2024-06-03 16:38:03 +0900

    GAMEOVREの画面完成、心臓のSE

--- Assets/池田/tenmetu.cs
+++ Assets/池田/tenmetu.cs
@@ -1,9 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class tenmetu: MonoBehaviour
 {
     // 点滅させる対象
-    [SerializeField] private SpriteRenderer _target;
+    [SerializeField] private Text _target;
 
     // 時系列のアルファ値の変化カーブ
     [SerializeField] private AnimationCurve _alphaCurve = AnimationCurve.Linear(0, 0, 1, 1);
@@ -23,12 +24,9 @@ public class tenmetu: MonoBehaviour
     private void Update()
     {
         // 内部時刻を経過させる
-        _time += Time.deltaTime;
+        _time += 0.001;
 
-        // 内部時刻をcycleで折り返す
-        if (_time > _cycle)
-            _time = Mathf.Repeat((float)_time, _cycle);
-
+       
         // アニメーションカーブに従ったアルファ値計算
         var alpha = _alphaCurve.Evaluate((float)_time);
 
